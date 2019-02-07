384319ef5e50ebb1614a4fd82047bb748ca6f332
Unknown <d.skich.games@gmail.com>
2019-02-07 21:11:23 +0200

    genetator +

--- generator/main.py
+++ generator/main.py
@@ -1,7 +1,14 @@
 import bitcoin.rpc
 import bitcoin
 from bitcoin.core import b2lx
-import sys, os, time, termios, tty, hashlib
+import sys, os, time, termios, tty, hashlib, signal
+
+def terminate(arg1, arg2):
+    print("[*]Shutdown...")
+    sys.exit(1)
+
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+signal.signal(signal.SIGINT, terminate)
 
 bitcoin.SelectParams("regtest")
 nodes = []
@@ -10,11 +17,14 @@ class Node():
         self.rpc_user = rpc_user
         self.rpc_password = rpc_password
         self.rpc_port = rpc_port
-        self.connect = bitcoin.rpc.Proxy(service_url="http://{}:{}@127.0.0.1:{}".format(
-            self.rpc_user, self.rpc_password, self.rpc_port))
-        self.connect.generate(101)
-        print("[*]Generating blocks...")
-        print("[*]Current balance for {} is {} BTC".format(self.rpc_user, self.check_balance()))
+
+    def rpc_connect(self):
+        try:
+            self.connect = bitcoin.rpc.Proxy(service_url="http://{}:{}@127.0.0.1:{}".format(
+                self.rpc_user, self.rpc_password, self.rpc_port))
+            return True
+        except:
+            return False
 
     def check_balance(self):
         balance = self.connect.getbalance()
@@ -22,8 +32,17 @@ class Node():
 
     def send_transaction(self, address, amount=100000):
         txid = self.connect.sendtoaddress(address,amount)
+        print("[*]Transaction: {} ---> {} ({} BTC)\nTXID: {}".format(
+            self.rpc_user, address, amount/pow(10,8), b2lx(txid)))
 
-        print("[*]Transaction: {} ---> {} ({} BTC)\nTXID: {}".format(self.rpc_user, address, amount/pow(10,8), b2lx(txid)))
+
+def get_revard(n=1):
+    print("[*]Mining...")
+    while n != 0:
+        n -= 1
+        for i in range(1,101):
+            for node in nodes:
+                node.connect.generate(1)
 
 #Проверка на наличие аргумента пути к папкам узлов
 if len(sys.argv) not in (2, 3):
@@ -45,16 +64,24 @@ for el in dir_el:
                 rpc_port = string.split("=")[1]
                 rpc_port = rpc_port[:len(rpc_port)-1]
         nodes.append(Node(rpc_user, rpc_password, rpc_port))
-
+        time.sleep(1)
 
 print("[*]Generator setup is complete\n"
-      "[*]Was found {} nodes\n"
-      "[*]Transaction formation begins...".format(str(len(nodes))))
+      "[*]Was found {} nodes".format(str(len(nodes))))
+
+input("Press ENTER to enable generator...")
+for node in nodes:
+    if node.rpc_connect():
+        print("[*]Connected to {}".format(node.rpc_user))
+    else:
+        print("[*]Node {} refuse connection".format(node.rpc_user))
+
+get_revard()
 
 while True:
     for node in nodes:
         recipients = list(nodes)
         recipients.remove(node)
-        for recipient in recipients:
-            rec_addr = recipient.connect.getnewaddress()
-            node.send_transaction(rec_addr)
+        for recipients in recipients:
+            time.sleep(0.33)
+            node.send_transaction(recipients.connect.getnewaddress())

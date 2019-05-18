f077f8914e7553ba594da41f38de24a7da2db151
Seal1998 <yazon.checkit@gmail.com>
2019-05-18 14:57:04 +0300

    + eos setup.sh

--- eos/setup.sh
+++ eos/setup.sh
@@ -49,4 +49,4 @@ echo "Complete output: ${IPADDRESS}"
 # set alias
 
 #shopt -s expand_aliases
-alias cleos='docker exec -it nodeos /opt/eosio/bin/cleos --url http://localhost:8888 --wallet-url http://${IPADDRESS%/*}:9876'
+alias cleos='sudo docker exec -it nodeos /opt/eosio/bin/cleos --url http://localhost:8888 --wallet-url http://${IPADDRESS%/*}:9876'
